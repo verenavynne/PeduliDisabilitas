e192c4dd33d2059226eba1e28373a9d8acc0160a
Vanness Ariya <vannessariya@gmail.com>
2024-11-14 20:51:17 +0700

    delete checkout.js

--- scripts/checkout.js
+++
@@ -1,6 +0,0 @@
-const dashboard = document.querySelector('dashboard-btn');
-const order = document.querySelector('order-btn');
-
-dashboard.addEventListener("submit", function(event) {
-    window.location.href = "home.html";
-}
