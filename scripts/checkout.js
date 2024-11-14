fdcf5047a8a446fe7350c56549931000ff2ed1cf
Vanness Ariya <vannessariya@gmail.com>
2024-11-14 20:46:36 +0700

    Update final checkout page

---
+++ scripts/checkout.js
@@ -0,0 +1,6 @@
+const dashboard = document.querySelector('dashboard-btn');
+const order = document.querySelector('order-btn');
+
+dashboard.addEventListener("submit", function(event) {
+    window.location.href = "home.html";
+}
